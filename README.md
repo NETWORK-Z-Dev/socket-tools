b1558da09d5b51ccbab6044a877b77556b442a78
Marcel <40896559+hackthedev@users.noreply.github.com>
2026-01-18 21:46:50 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# socket-tools
- 
+# Socket Toolbox
+
+This small and awesome library was made to simplify a basic socket.io setup by using an existing express server. In this example the express server will be setup with the [express-starter](#) library. The library will scan a directory for files that include socket.io handlers to keep everything modular without pain. Files inside the scanned directory that do not have the required socket handler block will fail to load and cause an error.
+
+```js
+import ExpressStarter from "@hackthedev/express-starter"
+import SocketTools from "@hackthedev/socket-tools"
+
+// express setup
+let starter = new ExpressStarter()
+starter.registerErrorHandlers();
+starter.registerTemplateMiddleware();
+starter.app.use(starter.express.static(starter.dirname + "/public"));
+starter.startHttpServer(5000);
+
+// Socket.io Setup
+let socketTools = new SocketTools({expressHttpServer: starter.server});
+socketTools.listen();
+```
+
+------
+
+## Setting up socket events
+
+On default the socket library will recursively scan for `.mjs` files inside the `./modules/sockets` folder relative from what `process.cwd()` returns. These files at a very minimum are required to have this structure:
+
+```js
+export default (io) => (socket) => {
+    socket.on('hello', function (data, response) {
+        response({ error: null, message: "Hi " + data.name })
+    });
+}
+```
+
+You can define multiple socket events and even functions inside or outside of the required `export default (io) => (socket) => {}`  block. You can do literally anything as long as the required block is present, because otherwise it will abort loading that file and logging it as error.
+
+```js
+function getLeftMessage(){
+    return ", have a great time!";
+}
+
+export default (io) => (socket) => {
+    socket.on('hello', function (data, response) {
+        response({ error: null, message: "Hi " + data.name })
+    });
+    
+    socket.on('bye', function (data, response) {
+        response({ error: null, message: "Bye " + data.name + getLeftMessage() })
+    });
+}
+```
+
+------
+
+## Custom sockets directory
+
+You can also specify a custom directory path to scan that for files instead. The default will always be `path.join(process.cwd(), "modules/sockets")`
+
+```js
+let socketTools = new SocketTools({expressHttpServer: starter.server, modulesDir: path.join(process.cwd(), "sockets")});
+```
+
+------
+
+## Non-socket files in scan directory
+
+If you have files inside the "scan directory" aka `modulesDir` then you can add the following line to the file to avoid the socket toolbox from reporting errors: `export default (io) => (socket) => {}`
+
+By leaving it empty it wont have any real effect and avoids errors and possible crashes.
---
+++ index.mjs
@@ -0,0 +1,109 @@
+import {Server} from "socket.io";
+import {pathToFileURL} from "url";
+import Logger from "@hackthedev/terminal-logger";
+import path from "path";
+import fs from "fs";
+
+export default class SocketTools {
+    constructor({
+                    expressHttpServer,
+                    maxHttpBufferSize,
+                    pingInterval,
+                    pingTimeout,
+                    modulesDir,
+                    cors = {}
+                } = {}) {
+
+        if (!expressHttpServer) throw new Error("SocketTools: expressHttpServer is required");
+        if (!maxHttpBufferSize) maxHttpBufferSize = 1e8;
+        if (!pingInterval) pingInterval = 25000;
+        if (!pingTimeout) pingTimeout = 60000;
+        if (!modulesDir) modulesDir = path.join(process.cwd(), "modules/sockets");
+
+        if (!cors?.origin) cors.origin = "*";
+        if (!cors?.methods) cors.methods = ["GET", "POST"];
+        if (!cors?.credentials) cors.credentials = false;
+
+        this.socketHandlers = [];
+        this.activeSockets = new Map();
+
+        this.io = new Server(expressHttpServer, {
+            maxHttpBufferSize,
+            secure: true,
+            pingInterval,
+            pingTimeout,
+            cors
+        });
+
+        this.loadSocketHandlers(this.modulesDir);
+    }
+
+    listen({
+               onDisconnect = null,
+               onConnection = null
+           } = {}) {
+
+        this.io.on("connection", async (socket) => {
+
+            this.registerSocketEvents(socket);
+            socket.on("disconnect", () => {
+
+            })
+        })
+    }
+
+    async loadSocketHandlers(mainHandlersDir) {
+        const fileList = [];
+
+        const scanDir = (dir) => {
+            const files = fs.readdirSync(dir, {withFileTypes: true});
+            for (const file of files) {
+                const filePath = path.join(dir, file.name);
+                if (file.isDirectory()) {
+                    scanDir(filePath);
+                }
+                else if (file.name.endsWith(".mjs")) {
+                    fileList.push(filePath);
+                }
+            }
+        };
+
+        scanDir(mainHandlersDir);
+
+        for (const filePath of fileList) {
+            const fileUrl = pathToFileURL(filePath).href;
+            try {
+                const {default: handlerFactory} = await import(fileUrl);
+                const handler = handlerFactory(this.io);
+
+                if (typeof handler === "function") {
+                    this.socketHandlers.push(handler);
+                    Logger.debug(`Preloaded socket handler: ${filePath}`);
+                }
+                else {
+                    Logger.warn(`Ignored invalid socket handler in ${filePath}`);
+                }
+            } catch (err) {
+                Logger.error(`Error importing socket handler: ${fileUrl}`);
+                Logger.error(err);
+            }
+        }
+    }
+
+    registerSocketEvents(socket) {
+        try {
+            const attachedHandlers = [];
+
+            for (const handler of this.socketHandlers) {
+                const cleanup = handler(socket);
+                if (typeof cleanup === "function") {
+                    attachedHandlers.push(cleanup);
+                }
+            }
+
+            this.activeSockets.set(socket.id, attachedHandlers);
+        } catch (err) {
+            console.error("Error registering socket events:", err);
+        }
+    }
+}
